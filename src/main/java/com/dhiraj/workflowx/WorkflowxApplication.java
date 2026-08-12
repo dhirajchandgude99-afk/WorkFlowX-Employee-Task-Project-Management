ddeb7b1cf95029578f350d3b9910609f5cd2add5
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-08-12 11:15:31 +0530

    Initial WorkFlowX project setup

---
+++ src/main/java/com/dhiraj/workflowx/WorkflowxApplication.java
@@ -0,0 +1,13 @@
+package com.dhiraj.workflowx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhiraj/workflowx/WorkflowxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhiraj.workflowx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

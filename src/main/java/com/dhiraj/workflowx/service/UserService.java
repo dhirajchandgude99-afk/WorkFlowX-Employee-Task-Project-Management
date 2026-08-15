4e2c90e8bd7b4e4fdc159e21cb1d8b6d4bab9715
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-08-15 20:42:32 +0530

    Add UserService CRUD operations

--- src/main/java/com/dhiraj/workflowx/service/UserService.java
+++ src/main/java/com/dhiraj/workflowx/service/UserService.java
@@ -1,7 +1,11 @@
 package com.dhiraj.workflowx.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.dhiraj.workflowx.entity.User;
 import com.dhiraj.workflowx.repository.UserRepository;
 
 @Service
@@ -11,5 +15,21 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
     }
 }

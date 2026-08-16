17634cb2a9889be7e9aeaa87632476372449739b
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-08-16 22:01:18 +0530

    Add ProjectService CRUD operations

--- src/main/java/com/dhiraj/workflowx/service/ProjectService.java
+++ src/main/java/com/dhiraj/workflowx/service/ProjectService.java
@@ -1,7 +1,11 @@
 package com.dhiraj.workflowx.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.dhiraj.workflowx.entity.Project;
 import com.dhiraj.workflowx.repository.ProjectRepository;
 
 @Service
@@ -11,5 +15,21 @@ public class ProjectService {
 
     public ProjectService(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
+    }
+
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public Optional<Project> getProjectById(Long id) {
+        return projectRepository.findById(id);
+    }
+
+    public Project saveProject(Project project) {
+        return projectRepository.save(project);
+    }
+
+    public void deleteProject(Long id) {
+        projectRepository.deleteById(id);
     }
 }

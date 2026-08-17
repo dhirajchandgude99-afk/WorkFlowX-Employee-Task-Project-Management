7ed8e277dd4e92e6c5aedc59d16942f0282d242f
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-08-17 19:31:24 +0530

    Add UserController PUT API

--- src/main/java/com/dhiraj/workflowx/controller/UserController.java
+++ src/main/java/com/dhiraj/workflowx/controller/UserController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +35,10 @@ public class UserController {
     @PostMapping
      public User createUser(@RequestBody User user) {
       return userService.saveUser(user);
+    }
+    @PutMapping("/{id}")
+     public User updateUser(@PathVariable Long id, @RequestBody User user) {
+     user.setId(id);
+     return userService.saveUser(user);
     }
 }

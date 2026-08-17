ef021a1015521adeba602dcded6a204d5b36e268
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-08-17 19:26:35 +0530

    Add UserController POST API

--- src/main/java/com/dhiraj/workflowx/controller/UserController.java
+++ src/main/java/com/dhiraj/workflowx/controller/UserController.java
@@ -2,6 +2,8 @@ package com.dhiraj.workflowx.controller;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,5 +31,9 @@ public class UserController {
     @GetMapping("/{id}")
     public Optional<User> getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
+    }
+    @PostMapping
+     public User createUser(@RequestBody User user) {
+      return userService.saveUser(user);
     }
 }

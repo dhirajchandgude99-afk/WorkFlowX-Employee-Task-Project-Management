67a54a9f3d401d348feda2840f8e115129a6c0d1
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-08-19 20:40:30 +0530

    Add TaskService exception handling

--- src/main/java/com/dhiraj/workflowx/controller/TaskController.java
+++ src/main/java/com/dhiraj/workflowx/controller/TaskController.java
@@ -1,7 +1,7 @@
 package com.dhiraj.workflowx.controller;
 
 import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,7 +31,7 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Task> getTaskById(@PathVariable Long id) {
+    public Task getTaskById(@PathVariable Long id) {
         return taskService.getTaskById(id);
     }
 
--- src/main/java/com/dhiraj/workflowx/service/TaskService.java
+++ src/main/java/com/dhiraj/workflowx/service/TaskService.java
@@ -1,11 +1,11 @@
 package com.dhiraj.workflowx.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.dhiraj.workflowx.entity.Task;
+import com.dhiraj.workflowx.exception.ResourceNotFoundException;
 import com.dhiraj.workflowx.repository.TaskRepository;
 
 @Service
@@ -21,8 +21,14 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
-    public Optional<Task> getTaskById(Long id) {
-        return taskRepository.findById(id);
+    public Task getTaskById(Long id) {
+
+        return taskRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Task not found with id: " + id
+                        )
+                );
     }
 
     public Task saveTask(Task task) {

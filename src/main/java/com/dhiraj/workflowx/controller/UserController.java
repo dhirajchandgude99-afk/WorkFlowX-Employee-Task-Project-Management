f4d5f99bd4b1195a3daf998de14d60e3d48828f0
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-08-27 20:08:25 +0530

    Document User APIs with Swagger

--- src/main/java/com/dhiraj/workflowx/controller/UserController.java
+++ src/main/java/com/dhiraj/workflowx/controller/UserController.java
@@ -2,7 +2,6 @@ package com.dhiraj.workflowx.controller;
 
 import java.util.List;
 
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +15,17 @@ import com.dhiraj.workflowx.dto.UserRequestDTO;
 import com.dhiraj.workflowx.dto.UserResponseDTO;
 import com.dhiraj.workflowx.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(
+    name = "User APIs",
+    description = "APIs for managing WorkflowX users"
+)
 public class UserController {
 
     private final UserService userService;
@@ -28,11 +34,19 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+        summary = "Get all users",
+        description = "Returns a list of all users"
+    )
     @GetMapping
     public List<UserResponseDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @Operation(
+        summary = "Get user by ID",
+        description = "Returns a user using the specified user ID"
+    )
     @GetMapping("/{id}")
     public UserResponseDTO getUserById(
             @PathVariable Long id) {
@@ -40,6 +54,10 @@ public class UserController {
         return userService.getUserById(id);
     }
 
+    @Operation(
+        summary = "Create a new user",
+        description = "Creates a new WorkflowX user"
+    )
     @PostMapping
     public UserResponseDTO createUser(
             @Valid @RequestBody UserRequestDTO request) {
@@ -47,6 +65,10 @@ public class UserController {
         return userService.createUser(request);
     }
 
+    @Operation(
+        summary = "Update a user",
+        description = "Updates an existing user using the specified user ID"
+    )
     @PutMapping("/{id}")
     public UserResponseDTO updateUser(
             @PathVariable Long id,
@@ -55,6 +77,10 @@ public class UserController {
         return userService.updateUser(id, request);
     }
 
+    @Operation(
+        summary = "Delete a user",
+        description = "Deletes an existing user using the specified user ID"
+    )
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable Long id) {
 

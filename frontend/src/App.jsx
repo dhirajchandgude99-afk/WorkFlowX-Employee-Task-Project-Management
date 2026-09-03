a038e12298bdb229e607b61d7f63c3c0f77c39de
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-09-03 20:16:47 +0530

    Create React frontend base structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>WorkflowX</h1>
+      <p>Employee Task & Project Management System</p>
+    </div>
+  )
+}
+
+export default App
